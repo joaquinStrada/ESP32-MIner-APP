bc91ff79e818efdc310652e9df437628c1497b6f
joaquin <joaquinstrada@hotmail.com.ar>
2024-06-15 12:57:54 -0300

    Terminada la autenticacion en el UserProvider

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -7,6 +7,7 @@ const UserProvider = ({ children }) => {
   const [AccessToken, setAccessToken] = useState(null)
   const [RefreshToken, setRefreshToken] = useState(null)
   const [ExpiresInRefreshToken, setExpiresInRefreshToken] = useState(null)
+  const [ExpiresInAccessToken, setExpiresInAccessToken] = useState(null)
 
   const updateAccessToken = async () => {
     if (new Date().getTime() > ExpiresInRefreshToken) return
@@ -26,6 +27,7 @@ const UserProvider = ({ children }) => {
       setAccessToken(accessToken)
       setRefreshToken(refreshToken)
       setExpiresInRefreshToken(expiresInRefreshToken + new Date().getTime() - 500)
+      setExpiresInAccessToken(expiresInAccessToken + new Date().getTime() - 500)
 
       // Actualizamos el local storage en caso de estar habilitado
       if (window.localStorage.getItem('refreshToken')) {
@@ -36,30 +38,31 @@ const UserProvider = ({ children }) => {
 
         window.localStorage.setItem('refreshToken', JSON.stringify(tokenStorage))
       }
-      setTimeout(updateAccessToken, expiresInAccessToken - 500)
     } catch (err) {
       console.error(err)
     }
   }
 
   const login = (accessToken, refreshToken, expiresInAccessToken, expiresInRefreshToken, remember) => {
-    if (remember) {
-      const localStrorage = {
-        refreshToken,
-        expiresIn: new Date().getTime() + expiresInRefreshToken - 500
-      }
-
-      window.localStorage.setItem('refreshToken', JSON.stringify(localStrorage))
+    const storageToken = {
+      refreshToken,
+      expiresIn: new Date().getTime() + expiresInRefreshToken - 500
     }
+
+    if (remember) {
+      window.localStorage.setItem('refreshToken', JSON.stringify(storageToken))
+    } else {
+      window.sessionStorage.setItem('refreshToken', JSON.stringify(storageToken))
+    }
+
     setAccessToken(accessToken)
     setRefreshToken(refreshToken)
     setExpiresInRefreshToken(expiresInRefreshToken + new Date().getTime() - 500)
-    setTimeout(updateAccessToken, expiresInAccessToken - 500)
+    setExpiresInAccessToken(expiresInAccessToken + new Date().getTime() - 500)
   }
 
   const getRefreshToken = () => {
-    const refreshToken = window.localStorage.getItem('refreshToken')
-
+    const refreshToken = window.localStorage.getItem('refreshToken') || window.sessionStorage.getItem('refreshToken')
     // Validamos que el token exista
     if (!refreshToken) return
 
@@ -71,13 +74,19 @@ const UserProvider = ({ children }) => {
     // Seteamos el nuevo token
     setRefreshToken(dataToken.refreshToken)
     setExpiresInRefreshToken(dataToken.expiresIn)
-    updateAccessToken()
   }
 
   useEffect(() => {
-    getRefreshToken()
+    if (RefreshToken === null) {
+      getRefreshToken()
+    } else if (RefreshToken && ExpiresInRefreshToken && ExpiresInAccessToken === null) {
+      updateAccessToken()
+    } else {
+      setTimeout(updateAccessToken, ExpiresInAccessToken - new Date().getTime())
+    }
   })
 
+  
   return (
     <UserContext.Provider value={{ AccessToken, login }}>
       {children}
